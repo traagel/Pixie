ba0976f2890b658261c50ab4e516f8c4721a5cc6
Mart Traagel <43983444+traagel@users.noreply.github.com>
2023-04-01 12:01:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -47,3 +47,12 @@ navItems.forEach((navItem) => {
     });
   });
 });
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
